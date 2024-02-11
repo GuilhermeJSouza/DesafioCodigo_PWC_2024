e9fe0059c3d50e664636881286921c9c8215f969
GuilhermeJSouza <gjds360@hotmail.com>
2024-02-11 18:10:43 -0300

    Reaprendendo Csharp

--- DesafioCodigo_PWC_2024/Form1.cs
+++ DesafioCodigo_PWC_2024/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,8 +20,23 @@ namespace DesafioCodigo_PWC_2024
 
         private void button_Confirmar_Click(object sender, EventArgs e)
         {
-            String enderecoDigitado = textBox_Entrada.Text;
-            MessageBox.Show(enderecoDigitado);
+            String enderecoDigitado = textBox_Entrada.Text;//Faz a leitura da entrada do usuário
+
+            //enderecoDigitado.Split(' ');
+            //string[] ruaNumero = new string[2];
+            //ruaNumero = enderecoDigitado.Split(' ');
+            
+            string[] ruaNumero = enderecoDigitado.Split(' ');// Dividindo a string no espaço em branco e atribuindo à um array
+
+            foreach (string parte in ruaNumero)//Imprimindo cada string do array
+            {
+                Console.WriteLine(parte);
+            }
+
+            //Console.WriteLine("Teste");
+            //Console.WriteLine(enderecoDigitado.Split(' '));
+
+            //MessageBox.Show(enderecoDigitado.Split(' '));
         }
     }
 }
