d512d4fe01fb0857c670bac8583966672789e0a7
GuilhermeJSouza <gjds360@hotmail.com>
2024-02-11 21:27:21 -0300

    Tentando Implementar Caso 2

--- DesafioCodigo_PWC_2024/Form1.cs
+++ DesafioCodigo_PWC_2024/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace DesafioCodigo_PWC_2024
 {
@@ -22,24 +23,44 @@ namespace DesafioCodigo_PWC_2024
         {
             String enderecoDigitado = textBox_Entrada.Text;//Faz a leitura da entrada do usuário
 
+            //##########################################     CASO 1     ########################################################
+
+            /*string[] ruaNumero = enderecoDigitado.Split(' ');// Dividindo a string no espaço em branco e atribuindo à um array
+
+            String rua = ruaNumero[0];
+
+            label_endereco_saida.Text = rua.Replace(",", "");//Atribui o texto à label de saída endereço removendo possíveis vírgulas
+            
+            label_numero_saida.Text = ruaNumero[1];//Atribui o texto à label de saída número*/
+
+            //##########################################     CASO 2     ###################################################################
+
             //enderecoDigitado.Split(' ');
             //string[] ruaNumero = new string[2];
             //ruaNumero = enderecoDigitado.Split(' ');
-            
-            string[] ruaNumero = enderecoDigitado.Split(' ');// Dividindo a string no espaço em branco e atribuindo à um array
 
-            foreach (string parte in ruaNumero)//Imprimindo cada string do array
+            //List<String> separadores = new List<String>();//Declara uma lista para as posições onde tem espaço na string de entrada
+
+            string[] endereco = new string[1];
+            string[] numero = new string[1];
+
+            //separadores.Add(0); // Inicia a lista
+
+            //int separadores = enderecoDigitado.IndexOf(" "); // adiciona na posição zero a posição do primeiro retorno de espaço
+
+            for (int x = enderecoDigitado.IndexOf(" "); x < enderecoDigitado.Length; x++) // Iteração para encontrar todos os espaços da
+                                                                          // frase
             {
-                Console.WriteLine(parte);
+                //if (enderecoDigitado.Substring(x, x + 1).Equals(" ") && enderecoDigitado.Substring(x + 1, x + 2).ToString() == "2")
+                if (enderecoDigitado[x] == ' ' && enderecoDigitado[x + 1] == '2')
+                {
+                    endereco[0] = enderecoDigitado.Substring(0, x);
+                    numero[0] = enderecoDigitado.Substring(x + 1, enderecoDigitado.Length-1-x);
+                    break;
+                }
             }
-
-            label_endereco_saida.Text = ruaNumero[0];//Atribui o texto à label de saída endereço
-            label_numero_saida.Text = ruaNumero[1];//Atribui o texto à label de saída número
-
-            //Console.WriteLine("Teste");
-            //Console.WriteLine(enderecoDigitado.Split(' '));
-
-            //MessageBox.Show(enderecoDigitado.Split(' '));
+            label_endereco_saida.Text = endereco[0];
+            label_numero_saida.Text = numero[0];
         }
     }
 }
